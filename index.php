759df66bf393985747c6aead141b2e2c0862528d
EgorStrupinskiy <venik2405@yandex.ru>
2022-03-26 13:14:29 +0300

    Directories errors

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 // index.php
-session_start();
-
-include "index/index.html";
-// If user is logged in, retrieve identity from session.
-$identity = null;
-if (isset($_SESSION['identity'])) {
-    $identity = $_SESSION['identity'];
-}
+	session_start();
+	
+	include "index/index.html";
+	// If user is logged in, retrieve identity from session.
+	$identity = null;
+	if (isset($_SESSION['identity'])) {
+	    $identity = $_SESSION['identity'];
+	}
 ?>
