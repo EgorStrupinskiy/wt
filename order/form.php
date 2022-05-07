7067b99ff3170dd63960b1f290d237e5fd2e6b2a
EgorStrupinskiy <venik2405@yandex.ru>
2022-05-07 11:23:19 +0300

    regular expressions fixed

--- order/form.php
+++ order/form.php
@@ -13,16 +13,24 @@ $words = explode($separator, $_POST['comments']);
 for ($i = 0; $i < count($words); $i++) {
     if (preg_match($regexp1, $words[$i], $match)) {
         $tmp = $match[0];
-        $tmp = substr_replace($tmp, $tmp[strlen($tmp) - 1] + 1, strlen($tmp) - 1, strlen($tmp) - 1);
+        $trash = explode('.', $tmp);
+        $value = intval($trash[2]);
+        $value++;
+        $tmp = $trash[0] . "." . $trash[1] . "." . $value;
+
         echo '<span style="color: red">' . $tmp . '</span>';
     } elseif (preg_match($regexp2, $words[$i], $match)) {
         $tmp = str_replace('/', '.', $match[0]);
-        $tmp = substr_replace($tmp, $tmp[strlen($tmp) - 1] + 1, strlen($tmp) - 1, strlen($tmp) - 1);
+        $trash = explode('.', $tmp);
+        $value = intval($trash[2]);
+        $value++;
+        $tmp = $trash[1] . "." . $trash[0] . "." . $value;
         echo '<span style="color: red">' . $tmp . '</span>';
     } else {
-        echo $words[$i];
+        if (strlen($words[$i]) == 0) {
+            echo ' ';
+        } else
+            echo $words[$i];
     }
     echo ' ';
-//    echo $words[$i];
 }
-// ^(0?[1-9]|[12][0-9]|3[01])[\/.](0?[1-9]|1[012])[\/.](\d{2}?(\d{2})?)$
